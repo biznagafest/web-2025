a09d9ca1d4192a6b16989dee5a8fa76db620b6ac
DavidRojoM <davidrojo1997@gmail.com>
2024-03-29 04:01:43 +0100

    feat: add astro insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercelStatic from "@astrojs/vercel/static";
+import pageInsight from "astro-page-insight";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), pageInsight()],
   output: "static",
-  adapter: vercelStatic(),
+  adapter: vercelStatic()
 });
